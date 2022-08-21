cd971b59676c04d2618172e9f60ed28a2ba39811
Yahya Alshalabi <yahya.alshalabi.work@gmail.com>
2022-08-21 15:34:38 +0200

    Create Project

---
+++ android/app/src/main/kotlin/sy/academy/najeeb/najeeb_academy/MainActivity.kt
@@ -0,0 +1,6 @@
+package sy.academy.najeeb.najeeb_academy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NajeebAcademyApp extends StatelessWidget {
+  const NajeebAcademyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+    );
+  }
+}
